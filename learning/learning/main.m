9a704ec185397f901a016d6c12b4fbd4c7a74357
harj <harjeet.taggar@gmail.com>
2012-02-24 17:47:20 -0800

    new files

---
+++ learning/learning/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  learning
+//
+//  Created by Harjeet Taggar on 2/24/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
